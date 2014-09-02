7279a190e747b8f8584c36d641e197b5ab220382
Ian Groff <ian@iangroff.com>
2014-09-02 11:23:22 -0500

    no message here

---
+++ server.js
@@ -0,0 +1,31 @@
+var express         = require('express');
+var morgan          = require('morgan');
+var cookieParser    = require('cookie-parser');
+var bodyParser      = require('body-parser');
+var connect         = require('connect');
+var connectTimeout  = require('connect-timeout');
+var log             = require('simplog');
+
+
+var app = express();
+
+app.use(connect());
+app.use(morgan('combined'));
+app.use(cookieParser());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+// parse application/vnd.api+json as json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
+
+
+// respond
+app.use(function(req, res, next){
+  res.send('Hello World');
+});
+
+listenPort = process.env.PORT || 3000;
+log.info("starting app " + process.env.APP_NAME);
+log.info("listening on " + listenPort);
+app.listen(listenPort);
